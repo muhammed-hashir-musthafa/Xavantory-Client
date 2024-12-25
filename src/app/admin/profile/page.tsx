5ea283178604e473f841577c09dce881fa384435
HashirMusthafa <hashirvaliyil8146@gmail.com>
2024-12-25 15:56:06 +0530

    Add Profile page under-admin/profile

---
+++ src/app/admin/profile/page.tsx
@@ -0,0 +1,12 @@
+import AdminProfilePage from "@/components/adminComponents/containers/ProfilePage/ProfilePage";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <AdminProfilePage />
+    </div>
+  );
+};
+
+export default Profile;
