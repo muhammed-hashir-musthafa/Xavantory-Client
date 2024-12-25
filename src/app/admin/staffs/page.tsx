4ff39cfb41e709a78e8f78406e721531f4e6ef28
HashirMusthafa <hashirvaliyil8146@gmail.com>
2024-12-25 15:56:50 +0530

    Create Staffs page under-admin/staffs

---
+++ src/app/admin/staffs/page.tsx
@@ -0,0 +1,12 @@
+import StaffTable from "@/components/staffComponents/containers/StaffTable/StaffTable";
+import React from "react";
+
+const Students = () => {
+  return (
+    <div>
+      <StaffTable />
+    </div>
+  );
+};
+
+export default Students;
